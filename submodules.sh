7bb17681a66571e293e0f57b62ebc7d3098c4e77
miermontoto <mier@mier.info>
2023-02-26 13:50:41 +0100

    update submodules script to include IFTTT webhook

--- submodules.sh
+++ submodules.sh
@@ -5,3 +5,6 @@ git submodule update --recursive --remote .
 git add .
 git commit -m "auto update submodules at $(date)"
 git push
+
+ifttt_url="https://maker.ifttt.com/trigger/submodules/with/key/${IFTTT_API_KEY}"
+curl -X POST $ifttt_url
