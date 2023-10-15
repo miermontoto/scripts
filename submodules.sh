3e722ae37db9c2bc084ff4a7aa533b472cb3a1f4
miermontoto <mier@mier.info>
2023-10-15 17:20:02 +0200

    fix backup update script

--- submodules.sh
+++ submodules.sh
@@ -1,9 +1,9 @@
 # Script that updates submodules
 cd ~/backup
 git pull
-git submodule update --recursive --remote .
+git submodule update --recursive --remote --init .
 git add .
-git commit -m "auto update submodules at $(date)"
+git commit -m "auto update submodules at $(date +'%Y-%m-%d %T')"
 git push
 
 ifttt_url="https://maker.ifttt.com/trigger/submodules/with/key/${IFTTT_API_KEY}"
