54ee8c79ebce4331c38d3c2dd82a000b0c053fe0
miermontoto <mier@mier.info>
2026-03-15 19:02:06 +0100

    fix(submodules): quitar ifttt y ajustar ruta

--- submodules.sh
+++ submodules.sh
@@ -1,10 +1,7 @@
 # Script that updates submodules
-cd ~/backup
+cd ~/uo/backup
 git pull
 git submodule update --recursive --remote --init .
 git add .
 git commit -m "auto update submodules at $(date +'%Y-%m-%d %T')"
 git push
-
-ifttt_url="https://maker.ifttt.com/trigger/submodules/with/key/${IFTTT_API_KEY}"
-curl -X POST $ifttt_url
